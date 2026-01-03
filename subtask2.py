37114157a32b0a8ca2c4c9d7d2c6e755c6a2b643
Leonardo <leonardo.affonso2@students.fh-wels.at>
2026-01-03 21:14:00 +0100

    readding the scripts (this time with the correct name)

---
+++ subtask2.py
@@ -0,0 +1,31 @@
+n = 0
+s = 0
+
+x = int(input())
+
+# loop
+while x != -1:
+
+    if n == 0:
+        m = x
+    else:
+        if x < m:
+            m = x
+
+    s = s + x
+    n = n + 1
+
+    x = int(input())
+
+# after loop
+if n == 0:
+    m = -1
+    a = -1
+else:
+    a = s / n
+
+# output
+print("n =", n)
+print("s =", s)
+print("m =", m)
+print("a =", a)
